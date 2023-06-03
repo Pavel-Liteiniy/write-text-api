d53d7fddff199a067086ab2e41a97b33ef7706d2
Liteiniy <paulsev96@gmail.com>
2023-06-03 18:37:42 +0700

    add: ThrottlerModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 import { UsersModule } from './users/users.module';
 import { OpenAiModule } from './open-ai/open-ai.module';
 import { PromptModule } from './prompt/prompt.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule, OpenAiModule, PromptModule],
+  imports: [
+    ConfigModule.forRoot(),
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 100,
+    }),
+    UsersModule,
+    OpenAiModule,
+    PromptModule,
+  ],
   controllers: [],
   providers: [],
 })
