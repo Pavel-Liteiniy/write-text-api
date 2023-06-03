c0d5af1c8489879a01f03a84106728e1be7e43c2
Liteiniy <paulsev96@gmail.com>
2023-06-03 18:37:55 +0700

    add: cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    cors: { origin: ['http://localhost:3001'] },
+    cors: { origin: ['http://localhost:3000', 'http://localhost:3001'] },
   });
   await app.listen(8080);
 }
